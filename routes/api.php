e4d525b12d1629b4168924c9cdb96c2e98b623cb
Daffa Pradana Tsaqif <160875540+Daffapradanat@users.noreply.github.com>
2024-06-06 09:05:29 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\bukucontroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/buku', [bukucontroller::class, 'index']);
+Route::post('/buku', [bukucontroller::class, 'store']);
+Route::get('/buku/{buku}', [bukucontroller::class, 'show']);
+Route::put('/buku/{buku}', [bukucontroller::class, 'update']);
+Route::delete('/buku/{buku}', [bukucontroller::class, 'destroy']);
