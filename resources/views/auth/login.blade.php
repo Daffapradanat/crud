567de3f43a9837aa659d513faf1a0d0b24075513
Daffapradanat <daffapradanatsaqif@gmail.com>
2024-06-07 11:08:58 +0700

    studi-kasus

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-body">
                     <form action="{{ route('authenticate') }}" method="post">
                         @csrf
-                        <div class="mb -3 row">
+                        <div class="mb-3 row">
                             <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
                             <div class="col-md-6">
                                 <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
