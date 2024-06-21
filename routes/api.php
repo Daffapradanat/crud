0f767dadefa9ec8e83d482ddb2dc2573183b4bef
Daffa Pradana Tsaqif <160875540+Daffapradanat@users.noreply.github.com>
2024-06-21 14:05:36 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('books', BookController::class);
-
-Route::post('register', [AuthenticationController::class, 'register']);
-Route::post('login', [AuthenticationController::class, 'login']);
-Route::get('me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
-Route::post('logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
